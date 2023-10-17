2a534a51c05f96e9f1ae5b806adcb1b38b81c7c2
bogdanpavl2000@gmail.com <50374356+LoliScripter@users.noreply.github.com>
2023-10-17 13:01:17 +0300

    Lab_1

---
+++ Lab_1/ConsoleApplication1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <queue>
+
+using namespace std;
+using namespace chrono;
+
+mutex mtx;
+condition_variable cv;
+queue<int> waiting_room;
+
+int max_seats = 5;
+const int num_customers = 10;
+int num_haircuts = 0;
+
+void simulate_barber_work() {
+    while (num_haircuts < num_customers) {
+        unique_lock<mutex> lock(mtx);
+
+        while (waiting_room.empty()) {
+            cout << "Barber is sleeping..." << endl;
+            cv.wait(lock);
+        }
+
+        int customer = waiting_room.front();
+
+        waiting_room.pop();
+        cout << "Barber is cutting hair for client..." << customer << endl;
+        num_haircuts++;
+
+        lock.unlock();
+        cv.notify_all();
+        this_thread::sleep_for(seconds(1));
+    }
+}
+
+void simulate_customer_action(int id) {
+    unique_lock<mutex> lock(mtx);
+
+    if (waiting_room.size() == max_seats) {
+        cout << "Client " << id << " leave because waiting room is full" << endl;
+        return;
+    }
+
+    waiting_room.push(id);
+    cout << "Client " << id << " in waiting room" << endl;
+
+    lock.unlock();
+    cv.notify_all();
+
+    this_thread::sleep_for(seconds(1));
+}
+
+int main() {
+    thread barber_thread(simulate_barber_work);
+    thread customer_threads[num_customers];
+
+    for (int i = 0; i < num_customers; i++) {
+        customer_threads[i] = thread(simulate_customer_action, i);
+        this_thread::sleep_for(milliseconds(100));
+    }
+
+    for (int i = 0; i < num_customers; i++) {
+        customer_threads[i].join();
+    }
+
+    barber_thread.join();
+
+    return 0;
+}
---
+++ Lab_1/OpenMPAppVariant.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <thread>
+#include <queue>
+#include <chrono>
+
+using namespace std;
+using namespace this_thread;
+using namespace chrono;
+
+int maxSeats = 5;
+int numOfClients = 10;
+int numOfHaircuts = 0;
+
+queue<int> waitingRoom;
+
+void SimulateBarberWork()
+{
+	while (numOfHaircuts < numOfClients)
+	{
+#pragma omp critical
+		if (waitingRoom.empty())
+		{
+			cout << "Barber is sleeping..." << endl;
+		}
+		else
+		{
+			int client = waitingRoom.front();
+			waitingRoom.pop();
+			cout << "Barber is curring hair for client " + client << endl;
+			numOfHaircuts++;
+		}
+
+		sleep_for(chrono::seconds(1));
+	}
+}
+
+void SimulateClientActions(int id)
+{
+#pragma omp critical
+	{
+		if (waitingRoom.size() == maxSeats)
+		{
+			cout << "Client " << id << " leave because waiting room is full" << endl;
+			return;
+		}
+
+		waitingRoom.push(id);
+		cout << "Client " << id << " in waiting room" << endl;
+	}
+
+	sleep_for(chrono::seconds(1));
+}
+
+int main()
+{
+#pragma omp parallel sections
+	{
+#pragma omp section
+		{
+			SimulateBarberWork();
+		}
+#pragma omp section
+		{
+			for (int i = 0; i < numOfClients; i++)
+			{
+				SimulateClientActions(i);
+			}
+		}
+
+	}
+
+	return 0;
+}
